7502a8b1a9ddb6d572e65086b59f6e143aa80265
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-05 15:29:37 -0300

    ✨ feat: Adiciona configuração da aplicação
    Configurações de Json, Cors, Url, Rotas e Porta

--- src/server.js
+++ src/server.js
@@ -9,3 +9,13 @@ mongoose.Promise = global.Promise;
 mongoose.connection.on("Error", (error) => {
   console.log(`Error: ${error.mensage}`);
 });
+
+const app = Express();
+app.use(cors());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+app.use("/", routers);
+
+const service = app.listen(process.env.PORT, () => {
+    console.log(`Server Startado na Porta ${service.address().port}`);
+})
