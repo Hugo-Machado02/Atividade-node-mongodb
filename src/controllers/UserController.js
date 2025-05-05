31db9aba4da77850bea85840346cb65748525021
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-05 14:40:00 -0300

    ✨ feat: adiciona busca de todos os usuários

---
+++ src/controllers/UserController.js
@@ -0,0 +1,8 @@
+const UserModel = require("../models/UserModel");
+
+module.exports = {
+    getUsers: async (req, res) => {
+        let users = await UserModel.find();
+        res.json({ users })
+    }
+}
