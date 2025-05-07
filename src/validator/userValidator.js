a4a39484288fe63513caa2f3e3f34632de794589
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-07 02:35:37 -0300

    ✨ feat: Adiciona a validação da criação de usuários

--- src/validator/userValidator.js
+++ src/validator/userValidator.js
@@ -1,7 +1,27 @@
 const { checkSchema } = require("express-validator");
 
 module.exports = {
-    validateUser: checkSchema({
+    addUser: checkSchema({
+        name: {
+            notEmpty: true,
+            isLength: {
+                options: { min: 3 },
+            },
+            errorMessage: "O mínimo para os nomes são de 3 caracteres",
+        },
+        email: {
+            isEmail: true,
+            normalizeEmail: true,
+            errorMessage: "email digitado invalido",
+        },
+        password: {
+            isLength: {
+                options: { min: 10 },
+            },
+            errorMessage: "A senha deve ter no mínimo 10 caracteres",
+        },
+    }),
+    editUser: checkSchema({
         id: {
             notEmpty: true,
             errorMessage: "O ID não pode ser vazio",
@@ -25,4 +45,3 @@ module.exports = {
             errorMessage: "A senha deve ter no mínimo 10 caracteres",
         },
   }),
-};
