f77f2fced4cede0d1da9db873f8f3b6744d134c9
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-07 02:05:42 -0300

    ✨ feat: Adiciona rota de edição do usuário

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,9 @@
 const Express = require("express");
 const UserController = require("../controller/UserController");
+const userValidator = require("../validator/userValidator");
 const router = Express.Router()
 
 router.get("/usuarios", UserController.getUsers);
+router.put("/usuariosUpdate",  userValidator.validateUser, UserController.editUser);
 
 module.exports = router;
