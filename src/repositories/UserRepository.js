64d7e150d78c89519542c5eea2132cfb17a8f5c9
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-18 21:21:17 -0300

    ✨ feat: Implementa o Repository para acesso ao bando dos Usuários

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,35 @@
+const UserModel = require("../models/UserModel");
+const mongoose = require("mongoose");
+
+class UserRepository {
+    async getAllUsers() {
+        return await UserModel.find();
+    }
+
+    async getUserId(id) {
+        return await UserModel.findById(id);
+    }
+
+    async getUserEmail(email) {
+        return await UserModel.findOne({ email: email })
+    }
+
+    async addUser(data) {
+        const newUser = new UserModel(data);
+        return await newUser.save();
+    }
+
+    async editUser(id, update){
+        return await UserModel.findByIdAndUpdate({ _id: id }, { $set: update })
+    }
+
+    async deleteUser(id){
+        return await UserModel.findByIdAndDelete(id)
+    }
+
+    async idUserValid(id) {
+        return mongoose.Types.ObjectId.isValid(id);
+    }
+}
+
+module.exports = new UserRepository();
