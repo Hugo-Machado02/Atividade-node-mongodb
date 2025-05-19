468df03194a720e635fce02166819fdf1cf54656
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-18 23:35:44 -0300

    ✨ feat: Configura Rotas da casa

--- src/routers/routersConfig.js
+++ src/routers/routersConfig.js
@@ -1,11 +1,13 @@
 const Express = require("express");
 const router = Express.Router();
 const userRouters = require("./userRouter");
+const casaRouters = require("./casaRouters");
 
 router.get("/service-test", (req, res) => {
     res.json({ return: "Rotas funcionando" })
 })
 
 router.use("/", userRouters);
+router.use("/", casaRouters);
 
 module.exports = router;
