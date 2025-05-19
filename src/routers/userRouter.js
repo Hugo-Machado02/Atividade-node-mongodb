877713be382ccc7e2f31b6efebcac60ab7e48e2c
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-18 21:22:56 -0300

    ♻️ refactor: Altera nome da função de busca de Usuários

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,7 +3,7 @@ const UserController = require("../controller/UserController");
 const userValidator = require("../validator/userValidator");
 const router = Express.Router()
 
-router.get("/usuarios", UserController.getUsers);
+router.get("/usuarios", UserController.getAllUsers);
 router.post("/usuarios/cadastro",  userValidator.addUser, UserController.addUser);
 router.put("/usuarios/edicao",  userValidator.editUser, UserController.editUser);
 router.delete("/usuarios/excluir",  userValidator.deleteUser, UserController.deleteUser);
